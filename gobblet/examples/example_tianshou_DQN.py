e0b5004be5a8b0d007741405eb1cfb0006e0d1aa
elliottower <etower@umass.edu>
2023-02-11 20:44:51 -0500

    Added self-play support for tianshou DQN, cleaned up training code

--- gobblet/examples/example_tianshou_DQN.py
+++ gobblet/examples/example_tianshou_DQN.py
@@ -17,6 +17,7 @@ from copy import deepcopy
 from typing import Optional, Tuple
 
 import gym
+import gymnasium
 import numpy as np
 import torch
 from tianshou.data import Collector, VectorReplayBuffer
@@ -62,6 +63,8 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument("--render_mode", type=str, default="human", choices=["human","rgb_array", "text", "text_full"], help="Choose the rendering mode for the game.")
     parser.add_argument("--debug", action="store_true", help="Flag to enable to print extra debugging info")
     parser.add_argument("--self_play", action="store_true", help="Flag to enable training via self-play (as opposed to fixed opponent)")
+    parser.add_argument("--self_play_generations", type=int, default=5, help="Number of generations of self-play agents to train (each generation can have multiple epochs of training)")
+    parser.add_argument("--self_play_greedy", action="store_true", help="Flag to have self-play train against a greedy agent for the first generation")
     parser.add_argument("--cpu-players", type=int, default=2, choices=[1, 2], help="Number of CPU players (options: 1, 2)")
     parser.add_argument("--player", type=int, default=0, choices=[0,1], help="Choose which player to play as: red = 0, yellow = 1")
     parser.add_argument("--record", action="store_true", help="Flag to save a recording of the game (game.gif)")
@@ -117,11 +120,11 @@ def get_agents(
     env = get_env()
     observation_space = (
         env.observation_space["observation"]
-        if isinstance(env.observation_space, gym.spaces.Dict)
+        if isinstance(env.observation_space, gym.spaces.Dict) or isinstance(env.observation_space, gymnasium.spaces.Dict)
         else env.observation_space
     )
     args.state_shape = (
-        observation_space["observation"].shape or observation_space["observation"].n
+        observation_space.shape or observation_space.n
     )
     args.action_shape = env.action_space.shape or env.action_space.n
     if agent_learn is None:
@@ -179,6 +182,139 @@ def get_env(render_mode=None, args=None):
     return PettingZooEnv(gobblet_v1.env(render_mode=render_mode, args=args))
 
 
+def train_selfplay(
+    args: argparse.Namespace = get_args(),
+    agent_learn: Optional[BasePolicy] = None,
+    agent_fixed: Optional[BasePolicy] = None,
+):
+    # always train first agent, start from random policy
+
+    # ======== environment setup =========
+    train_envs = DummyVectorEnv([get_env for _ in range(args.training_num)])
+    test_envs = DummyVectorEnv([get_env for _ in range(args.test_num)])
+    # seed
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    train_envs.seed(args.seed)
+    test_envs.seed(args.seed)
+
+    # ======== environment space =========
+    # model
+    env = get_env()
+    observation_space = (
+        env.observation_space["observation"]
+        if isinstance(env.observation_space, gymnasium.spaces.Dict) or isinstance(env.observation_space, gym.spaces.Dict)
+        else env.observation_space
+    )
+    args.state_shape = (
+            observation_space.shape or observation_space.n
+    )
+    args.action_shape = env.action_space.shape or env.action_space.n
+
+    # ======== model setup =========
+    # Note: custom models can be specified using the agent_fixed and agent_learn arguments
+    if agent_learn is None:
+        net = Net(args.state_shape, args.action_shape,
+                  hidden_sizes=args.hidden_sizes, device=args.device
+                  ).to(args.device)
+        optim = torch.optim.Adam(net.parameters(), lr=args.lr)
+        agent_learn = DQNPolicy(
+            net, optim, args.gamma, args.n_step,
+            target_update_freq=args.target_update_freq)
+
+    if agent_fixed is None:
+        net_fixed = Net(args.state_shape, args.action_shape,
+                        hidden_sizes=args.hidden_sizes, device=args.device
+                        ).to(args.device)
+        optim_fixed = torch.optim.SGD(net_fixed.parameters(), lr=0)
+        agent_fixed = DQNPolicy(
+            net_fixed, optim_fixed, args.gamma, args.n_step,
+            target_update_freq=args.target_update_freq)
+
+    # Load initial opponent from file
+    # path = os.path.join(args.logdir, 'gobblet', 'dqn', 'policy.pth')
+    # agent_fixed.load_state_dict(torch.load(path))
+
+    # ======== agent setup =========
+    agents = [agent_learn, agent_fixed]
+    policy = MultiAgentPolicyManager(agents, env)
+    agents_list = list(policy.policies.keys())
+
+
+    # ======== collector setup =========
+    train_collector = Collector(
+        policy,
+        train_envs,
+        VectorReplayBuffer(args.buffer_size, len(train_envs)),
+        exploration_noise=True,
+    )
+    test_collector = Collector(policy, test_envs, exploration_noise=True)
+    # policy.set_eps(1)
+    train_collector.collect(n_step=args.batch_size * args.training_num)
+
+    # ======== tensorboard logging setup =========
+    log_path = os.path.join(args.logdir, "gobblet", "dqn-selfplay")
+    writer = SummaryWriter(log_path)
+    writer.add_text("args", str(args))
+    logger = TensorboardLogger(writer)
+
+    def save_best_fn(policy):
+        if hasattr(args, "model_save_path"):
+            model_save_path = args.model_save_path
+        else:
+            model_save_path = os.path.join(
+                args.logdir, "gobblet", "dqn-selfplay", "policy.pth"
+            )
+        torch.save(
+            policy.policies[agents_list[args.agent_id - 1]].state_dict(), model_save_path
+        )
+
+    def stop_fn(mean_rewards):
+        return mean_rewards >= args.win_rate
+
+    def train_fn(epoch, env_step):
+        policy.policies[agents_list[0]].set_eps(args.eps_train)
+
+        if hasattr(policy.policies[agents_list[1]], "set_eps"):
+            policy.policies[agents_list[1]].set_eps(args.eps_train)
+
+    def test_fn(epoch, env_step):
+        policy.policies[agents_list[0]].set_eps(args.eps_test)
+
+        if hasattr(policy.policies[agents_list[1]], "set_eps"):
+            policy.policies[agents_list[1]].set_eps(args.eps_test)
+
+    def reward_metric(rews):
+        return rews[:, 0]
+
+
+    # Self-play loop
+    for i in range(args.self_play_generations):
+        result = offpolicy_trainer(
+            policy, train_collector, test_collector, args.epoch,
+            args.step_per_epoch, args.step_per_collect, args.test_num,
+            args.batch_size, train_fn=train_fn, test_fn=test_fn,
+            stop_fn=stop_fn, save_best_fn=save_best_fn, update_per_step=args.update_per_step,
+            logger=logger, test_in_train=False, reward_metric=reward_metric)
+
+        print(f"Launching game between learned agent ({type(policy.policies[agents_list[0]]).__name__}) and fixed agent ({type(policy.policies[agents_list[1]]).__name__}):")
+        # Render a single game between the learned policy and fixed policy from last generation
+        watch_selfplay(args, policy.policies[agents_list[0]])
+
+        # Set fixed opponent policy as to the current trained policy (updates every epoch)
+        policy.policies[agents_list[1]] = deepcopy(policy.policies[agents_list[0]])
+        print('--- SELF-PLAY GENERATION: {} ---'.format(i + 1))
+
+        print(f"Launching game between learned agent ({type(policy.policies[agents_list[0]]).__name__}) and itself ({type(policy.policies[agents_list[1]]).__name__}):")
+        # Render a single game between the learned policy and itself
+        watch_selfplay(args, policy.policies[agents_list[0]])
+
+    model_save_path = os.path.join(args.logdir, 'gobblet', 'dqn-selfplay', 'policy.pth')
+    torch.save(policy.policies[agents_list[0]].state_dict(), model_save_path)
+
+    return result, policy.policies[agents_list[0]]
+
+
 def train_agent(
     args: argparse.Namespace = get_args(),
     agent_learn: Optional[BasePolicy] = None,
@@ -198,6 +334,7 @@ def train_agent(
     policy, optim, agents = get_agents(
         args, agent_learn=agent_learn, agent_opponent=agent_opponent, optim=optim
     )
+    agents_list = list(policy.policies.keys())
 
     # ======== collector setup =========
     train_collector = Collector(
@@ -225,26 +362,17 @@ def train_agent(
                 args.logdir, "gobblet", "dqn", "policy.pth"
             )
         torch.save(
-            policy.policies[agents[args.agent_id - 1]].state_dict(), model_save_path
+            policy.policies[agents_list[args.agent_id - 1]].state_dict(), model_save_path
         )
 
     def stop_fn(mean_rewards):
         return mean_rewards >= args.win_rate
 
     def train_fn(epoch, env_step):
-        policy.policies[agents[args.agent_id - 1]].set_eps(args.eps_train)
-
-    def train_fn_selfplay(epoch, env_step):
-        policy.policies[agents[0]].set_eps(args.eps_train) # Same as train_fn but for both agents instead of only learner
-        policy.policies[agents[1]].set_eps(args.eps_train)
+        policy.policies[agents_list[args.agent_id - 1]].set_eps(args.eps_train)
 
     def test_fn(epoch, env_step):
-        policy.policies[agents[args.agent_id - 1]].set_eps(args.eps_test)
-
-    def test_fn_selfplay(epoch, env_step):
-        policy.policies[agents[0]].set_eps(args.eps_test) # Same as test_fn but for both agents instead of only learner
-        policy.policies[agents[1]].set_eps(args.eps_test)
-
+        policy.policies[agents_list[args.agent_id - 1]].set_eps(args.eps_test)
 
     def reward_metric(rews):
         return rews[:, args.agent_id - 1]
@@ -259,8 +387,8 @@ def train_agent(
         args.step_per_collect,
         args.test_num,
         args.batch_size,
-        train_fn=train_fn_selfplay if args.self_play else train_fn,
-        test_fn=test_fn_selfplay if args.self_play else test_fn,
+        train_fn=train_fn,
+        test_fn=test_fn,
         stop_fn=stop_fn,
         save_best_fn=save_best_fn,
         update_per_step=args.update_per_step,
@@ -269,7 +397,7 @@ def train_agent(
         reward_metric=reward_metric,
     )
 
-    return result, policy.policies[agents[args.agent_id - 1]]
+    return result, policy.policies[agents_list[args.agent_id - 1]]
 
 
 # ======== a test function that tests a pre-trained agent ======
@@ -282,12 +410,10 @@ def watch(
     policy, optim, agents = get_agents(
         args, agent_learn=agent_learn, agent_opponent=agent_opponent
     )
+    agents_list = list(policy.policies.keys())
+
     policy.eval()
-    if not args.self_play:
-        policy.policies[agents[args.agent_id - 1]].set_eps(args.eps_test)
-    else:
-        policy.policies[agents[0]].set_eps(args.eps_test)
-        policy.policies[agents[1]].set_eps(args.eps_test)
+    policy.policies[agents_list[args.agent_id - 1]].set_eps(args.eps_test)
 
     collector = Collector(policy, env, exploration_noise=True)
     pettingzoo_env = env.workers[0].env.env  # DummyVectorEnv -> Tianshou PettingZoo Wrapper -> PettingZoo Env
@@ -306,11 +432,24 @@ def watch(
 
         if collector.data.terminated or collector.data.truncated:
             rews, lens = result["rews"], result["lens"]
-            print(f"Final reward: {rews[:, 0].mean()}, length: {lens.mean()} [{type(policy.policies[agents[0]]).__name__}]")
-            print(f"Final reward: {rews[:, 1].mean()}, length: {lens.mean()} [{type(policy.policies[agents[1]]).__name__}]")
+            print(f"Final reward: {rews[:, 0].mean()}, length: {lens.mean()} [{type(policy.policies[agents_list[0]]).__name__}]")
+            print(f"Final reward: {rews[:, 1].mean()}, length: {lens.mean()} [{type(policy.policies[agents_list[1]]).__name__}]")
             if recorder is not None:
                 recorder.end_recording(pettingzoo_env.unwrapped.screen)
                 recorder = None
+
+def watch_selfplay(
+        args: argparse.Namespace = get_args(),
+        agent: Optional[BasePolicy] = None,
+) -> None:
+    # env = get_env(render_mode=args.render_mode, args=args)
+    env = DummyVectorEnv([lambda: get_env(render_mode=args.render_mode, args=args)])
+    policy = MultiAgentPolicyManager(policies=[agent, deepcopy(agent)], env=get_env(render_mode=args.render_mode, args=args)) # fixed here
+    policy.eval()
+    collector = Collector(policy, env, exploration_noise=True)
+    result = collector.collect(n_episode=1, render=True)
+    rews, lens = result["rews"], result["lens"]
+    print(f"Final reward: {rews[:, 0].mean()}, length: {lens.mean()}")
 
 # ======== allows the user to input moves and play vs a pre-trained agent ======
 def play(
@@ -323,8 +462,9 @@ def play(
     policy, optim, agents = get_agents(
         args, agent_learn=agent_learn, agent_opponent=agent_opponent
     )
+    agents_list = list(policy.policies.keys())
     policy.eval()
-    policy.policies[agents[args.agent_id - 1]].set_eps(args.eps_test)
+    policy.policies[agents_list[args.agent_id - 1]].set_eps(args.eps_test)
 
     # Experimental: let the CPU agent to continue training (TODO: check if this actually changes things meaningfully)
     # policy.policies[agents[args.agent_id - 1]].set_eps(args.eps_train)
@@ -366,10 +506,22 @@ if __name__ == "__main__":
     args = get_args()
     if args.player == 1:
         args.agent_id = 1 # Ensures trained agent is in the  correct spot
-    print("Training agent...")
-    result, agent = train_agent(args)
-    print("Starting game...")
-    if args.cpu_players == 2:
-        watch(args, agent)
+
+    if args.self_play:
+        print("Training agent...")
+
+        # Hard code the first fixed agent to be the greedy policy (after one generation it will be switched to a copy of the learned agent)
+        agent_fixed = GreedyPolicy() if args.self_play_greedy else None
+        result, agent = train_selfplay(args=args, agent_fixed=agent_fixed) # Hard code the first fixed agent to be the greedy policy
+
+        print("Starting game...")
+        watch_selfplay(args, agent)
     else:
-        play(args, agent)
+        print("Training agent...")
+        result, agent = train_agent(args)
+
+        print("Starting game...")
+        if args.cpu_players == 2:
+            watch(args, agent)
+        else:
+            play(args, agent)
