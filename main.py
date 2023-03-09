a198caf8d35ce8842ecf566540e411685f774016
elliottower <elliot@elliottower.com>
2023-03-09 17:52:57 -0500

    Updated main.py to run for a loop after first game finishes

--- main.py
+++ main.py
@@ -15,40 +15,44 @@ RENDER_MODE = "human"
 
 async def main() -> None:
     env = gobblet_v1.env(render_mode="human", args=None)
-    env.reset()
-    env.render()  # need to render the environment before pygame can take user input
 
     greedy_policy = gobblet_v1.GreedyGobbletPolicy(depth=DEPTH)
 
-    iter = 0
+    # Continue to display games between greedy agents
+    while True:
+        env.reset()
+        env.render()  # need to render the environment before pygame can take user input
 
-    for agent in env.agent_iter():
-        observation, reward, termination, truncation, info = env.last()
 
-        if termination or truncation:
-            print(f"Agent: ({agent}), Reward: {reward}, info: {info}")
-            break
+        iter = 0
 
-        if iter < 5:
-            # Randomize the first action (otherwise both agents will make the same moves and get stuck in a loop)
-            action_mask = observation["action_mask"]
-            action = np.random.choice(
-                np.arange(len(action_mask)), p=action_mask / np.sum(action_mask)
-            )
-            # Wait 1 second between moves so the user can follow the sequence of moves
-            time.sleep(1)
+        for agent in env.agent_iter():
+            observation, reward, termination, truncation, info = env.last()
 
-        else:
-            action = greedy_policy.compute_action(
-                observation["observation"], observation["action_mask"]
-            )
-            # Wait 1 second between moves so the user can follow the sequence of moves
-            time.sleep(1)
+            if termination or truncation:
+                print(f"Agent: ({agent}), Reward: {reward}, info: {info}")
+                break
 
-        env.step(action)
+            if iter < 5:
+                # Randomize the first 4 actions (otherwise both agents will make the same moves and get stuck in a loop)
+                action_mask = observation["action_mask"]
+                action = np.random.choice(
+                    np.arange(len(action_mask)), p=action_mask / np.sum(action_mask)
+                )
+                # Wait 1 second between moves so the user can follow the sequence of moves
+                time.sleep(1)
 
-        await asyncio.sleep(0)  # Very important, and keep it 0
-        iter += 1
+            else:
+                action = greedy_policy.compute_action(
+                    observation["observation"], observation["action_mask"]
+                )
+                # Wait 1 second between moves so the user can follow the sequence of moves
+                time.sleep(1)
+
+            env.step(action)
+
+            await asyncio.sleep(0)  # Very important, and keep it 0
+            iter += 1
 
 
 if __name__ == "__main__":
